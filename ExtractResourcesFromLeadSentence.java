06dc619a30905529c47a13bdb00486eb75e9672c
kk0510 <kousuke_k0510@yahoo.co.jp>
2012-10-01 19:02:45 +0900

    記事ごとの定義文(1文目)を表示する

---
+++ ExtractResourcesFromLeadSentence.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package extractfromleadsentence;
+
+/**
+ *
+ * @author kosuke
+ */
+public class ExtractResourcesFromLeadSentence {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		CIRelations a = new CIRelations();
+		a.ExtractCIRelations();
+	//a.CreateFeatureOfAnswerType(null);
+	
+	}
+}
