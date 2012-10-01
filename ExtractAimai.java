39ae022e015f41ba0330f6ba1604f027cc0fcaaf
kk0510 <kousuke_k0510@yahoo.co.jp>
2012-10-01 19:13:59 +0900

    Update ExtractAimai.java

--- ExtractAimai.java
+++ ExtractAimai.java
@@ -2,6 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+ /* 27734の曖昧さ回避ページがある */
 package extractfromleadsentence;
 
 import java.io.BufferedReader;
